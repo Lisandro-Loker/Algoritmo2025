c6102427e33d2a617c0b08f089c63114be2bc5d5
Lisandro-Loker <lokerlisandro@gmail.com>
2025-08-18 19:44:38 -0300

    busquedas en las anotaciones

--- Arboles/Anotaciones.py
+++ Arboles/Anotaciones.py
@@ -6,11 +6,12 @@
 # Cada parte del arbol es un sub arbol.
 # La raiz es el primer nodo.
 
-from typing import Any
+from typing import Any, Optional
 
 class _nodeTree:
-    def __init__(self, value: Any):
+    def __init__(self, value: Any, other_values: Optional[Any] = None):
         self.value = value
+        self.other_values = other_values
         self.left = None
         self.right = None
 
@@ -21,7 +22,7 @@ class Tree:
     def insert(self, value: Any):
 
         def __insert(root, value):
-            print(f'insertar value {value}')
+            print(f'insertar valor {value}')
             if root is None:
                 print ("Insertar Raiz")
                 return _nodeTree(value)
@@ -35,6 +36,49 @@ class Tree:
             return root
         
         self.root = __insert(self.root, value)
+        
+    def pre_order(self):
+        def __pre_order(root):
+            if root is not None:
+                print(root.value)
+                __pre_order(root.left)
+                __pre_order(root.right)
+
+        if self.root is not None:
+            __pre_order(self.root)
+
+    def in_order(self):
+        def __in_order(root):
+            if root is not None:
+                __in_order(root.left)
+                print(root.value)
+                __in_order(root.right)
+        if self.root is not None:
+            __in_order(self.root)
+
+    def post_order(self):
+        def __post_order(root):
+            if root is not None:
+                __post_order(root.right)
+                print(root.value)
+                __post_order(root.left)
+        if self.root is not None:
+            __post_order(self.root)
+
+    def search(self, value: Any):
+        def __search(root, value):
+            if root is not None:
+                if root.value == value:
+                    return root
+                elif root.value < value:
+                    return __search(root.right, value)
+                else:
+                    return __search(root.right, value)
+
+        Aux = None        
+        if self.root is not None:
+            Aux = __search(self.root, value)
+        return Aux    
 
 
 arbol = Tree()
@@ -43,5 +87,10 @@ arbol.insert(19)
 arbol.insert(7)
 arbol.insert(31)
 arbol.insert(11)
+arbol.insert(19)
+arbol.insert(27)
+arbol.insert(45)
 
-print(arbol.root.value, arbol.root.left.value, arbol.root.right.value)
+arbol.in_order()
+arbol.post_order()
+arbol.pre_order()
